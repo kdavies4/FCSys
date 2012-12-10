19973f147df85dc1df09ee9b8e6d698c8bb63b8b
Kevin Davies <kdavies4@gmail.com>
2012-12-10 18:15:56 -0500

    Added missing images

--- 00-process-gh-pages.py
+++ 00-process-gh-pages.py
@@ -12,19 +12,14 @@ import re, glob, sys, os
 # Replacement pairs
 rpls = [
     # Update the image links.
-    ('<img src="/[A-Za-z0-9.]+/', '<img src="'),
-    ('<img src="[A-Za-z0-9.]+/', '<img src="'), # Repeated for subdirectories
-    ('<img src="[A-Za-z0-9.]+/', '<img src="'),
-    ('<img src="[A-Za-z0-9.]+/', '<img src="'),
-    ('<img src="[A-Za-z0-9.]+/', '<img src="'),
-    ('<img src="[A-Za-z0-9.]+/', '<img src="'),
-    ('<img src="[A-Za-z0-9.]+/', '<img src="'),
-    ('<img src="[A-Za-z0-9.]+/', '<img src="'),
-    ('([A-Za-z0-9.]+)\.png', r'images/\1.png'),
-    ('([A-Za-z0-9.]+)\.svg', r'images/\1.svg'),
-    ('([A-Za-z0-9.]+)\.gif', r'images/\1.gif'),
-    ('\.\./resources/images/(favicon\.ico)', r'images/\1'),
-    ('\.\./resources/documentation/UsersGuide/References/(Natural Unit Representation in Modelica \(poster\)\.pdf)', r'images/\1'),
+    ('<img src="[A-Za-z0-9._]+/[A-Za-z0-9._]+/[A-Za-z0-9._]+/[A-Za-z0-9._]+/[A-Za-z0-9._]+/', '<img src="'),
+    ('<img src="[A-Za-z0-9._]+/[A-Za-z0-9._]+/', '<img src="'), # Repeated for subdirectories
+    ('<img src="[A-Za-z0-9._]+/', '<img src="'),
+    ('([A-Za-z0-9._]+)\.png', r'images/\1.png'),
+    ('([A-Za-z0-9._]+)\.svg', r'images/\1.svg'),
+    ('([A-Za-z0-9._]+)\.gif', r'images/\1.gif'),
+    ('\.\./resources/documentation/(favicon\.ico)', r'images/\1'),
+    ('\.\./resources/documentation/(.+\.pdf)', r'images/\1'),
     # FCSys.html will be index.html.
     ('"FCSys\.html"', '"index.html"'),
     # Change the title of the main page.
@@ -49,17 +44,17 @@ rpls = [
     # **Temporary:  Add note to the sidebar.
     ("""<h3>Table of Contents</h3>
     <ul>
-    <li><a href="FCSys_UsersGuide.html">User's Guide</a></li>
-    <li><a href="FCSys_BCs.html">BCs</a></li>
-    <li><a href="FCSys_Sensors.html">Sensors</a></li>
-    <li><a href="FCSys_Assemblies.html">Assemblies</a></li>
-    <li><a href="FCSys_Regions.html">Regions</a></li>
-    <li><a href="FCSys_Subregions.html">Subregions</a></li>
-    <li><a href="FCSys_Connectors.html">Connectors</a></li>
-    <li><a href="FCSys_Characteristics.html">Characteristics</a></li>
-    <li><a href="FCSys_Units.html">Units</a></li>
-    <li><a href="FCSys_Quantities.html">Quantities</a></li>
-    <li><a href="FCSys_BaseClasses.html">BaseClasses</a></li>
+    <li><a href="FCSys_UsersGuide\.html">User's Guide</a></li>
+    <li><a href="FCSys_BCs\.html">BCs</a></li>
+    <li><a href="FCSys_Sensors\.html">Sensors</a></li>
+    <li><a href="FCSys_Assemblies\.html">Assemblies</a></li>
+    <li><a href="FCSys_Regions\.html">Regions</a></li>
+    <li><a href="FCSys_Subregions\.html">Subregions</a></li>
+    <li><a href="FCSys_Connectors\.html">Connectors</a></li>
+    <li><a href="FCSys_Characteristics\.html">Characteristics</a></li>
+    <li><a href="FCSys_Units\.html">Units</a></li>
+    <li><a href="FCSys_Quantities\.html">Quantities</a></li>
+    <li><a href="FCSys_BaseClasses\.html">BaseClasses</a></li>
     </ul>""",
     """<h3>Table of Contents</h3>
     <ul>
