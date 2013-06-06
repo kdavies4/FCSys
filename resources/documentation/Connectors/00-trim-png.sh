74c5b0934632f6c1b538f733b80d658cf5b23dda
Kevin Davies <kdavies4@gmail.com>
2013-06-06 08:41:28 -0400

    Auto-updated github pages

--- resources/documentation/Connectors/00-trim-png.sh
+++ resources/documentation/Connectors/00-trim-png.sh
@@ -1,1 +1,16 @@
-../00-trim-png.sh
+#!/bin/bash
+# Trim the blank space around the edges of all the PNG images in this
+# directory (recursive), then add a white border.
+#
+# This requires ImageMagick (http://www.imagemagick.org).
+# This replaces the orginal images!
+# It's best not to use this procedure on vector graphics images because
+# ImageMagick will render them.
+
+IFS=$'\n' # Allow spaces in filenames.
+for f in `find -iname '*.png'`; do
+    echo Processing $f...
+    mogrify -fuzz 2% -trim "$f"
+    #mogrify -bordercolor '#FFFFFF' -border '10x10' -gravity center "$f"
+    mogrify -bordercolor '#FFFFFF' -border '10x10' "$f"
+done
